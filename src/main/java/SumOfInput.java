a8da772ace7a417afec63c3b6dea9618f115760a
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-02-28 12:54:28 -0500

    Gaussian method

--- src/main/java/SumOfInput.java
+++ src/main/java/SumOfInput.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 
-class SumOfInput{
-   public static void main (String[] args){
-       Scanner scanner = new Scanner(System.in);
+class SumOfInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-       System.out.print("Enter a number here: ");
-       int num = scanner.nextInt();
-       System.out.println("Some number " + sumNumbers(num));
-   }
-   public static int sumNumbers(int n) {
+        System.out.print("Enter a number here: ");
+        int num = scanner.nextInt();
+        System.out.println("Some number " + sumNumbers(num));
+        System.out.println("Some number " + sumGuassian(num));
+    }
+
+    public static int sumNumbers(int n) {
         int sum = 0;
         for (int i = 1; i <= n; i++) {
             sum = sum + i;
         }
         return sum;
     }
+
+
+    public static int sumGuassian(int n) {
+        int total = 0;
+//        for (int i = 1; i <= n; i++) {
+            total = (n * (n + 1)) / 2;
+      //  }
+        return total;
+    }
 }
+
+
+
+
+
+
 
